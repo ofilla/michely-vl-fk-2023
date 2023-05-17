935fe7cf4f9d7b139a727c7960c8daba0b6ddedb
Oliver Filla <ocl@hotmail.de>
2023-05-17 13:21:23 +0200

    restructure

--- templates/export.sh
+++ templates/export.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cat ??_*.md > Festkörperphysik.md || exit $?
+cat Mitschriften/??_*.md > Festkörperphysik.md || exit $?
 sed -i 's/braket/expval/g' Festkörperphysik.md
 pandoc --defaults templates/defaults.yml --top-level-division=chapter $@ || exit $?
-sed -i 's/expval/braket/g' Festkörperphysik.md || exit $?
+cat Mitschriften/??_*.md > Festkörperphysik.md || exit $?
